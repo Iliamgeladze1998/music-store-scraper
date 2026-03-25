038c584c67e5e6c1dfdfd8d1ee2f7c414484d124
iliamgeladze1998 <mgeladzeilia39@gmail.com>
2026-03-25 23:28:52 +0400

    fixed headless mode in both link scrapers

--- geovoice_get_links.py
+++ geovoice_get_links.py
@@ -3,7 +3,7 @@ from playwright.async_api import async_playwright
 
 async def get_geovoice_all_pages():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         context = await browser.new_context(viewport={'width': 1920, 'height': 1080})
         page = await context.new_page()
 
--- get_links.py
+++ get_links.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 def get_all_subcategory_links():
     with sync_playwright() as p:
         # headless=False so you can see the menu navigation in real-time
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(viewport={'width': 1920, 'height': 1080})
         page = context.new_page()
         
